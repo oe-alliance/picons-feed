c0e6504824736207009da2d790020d16858146a8
Captain <captain.onboard@web.de>
2023-07-04 22:56:39 +0200

    change version to date only

--- feed/ipkg-make-index.sh
+++ feed/ipkg-make-index.sh
@@ -9,10 +9,18 @@ for pkg in $(find . -name '*.ipk' | sort); do
     file_size=$(ls -l --dereference $pkg | awk '{print $5}')
     md5sum=$(md5sum $pkg | awk '{print $1}')
     pkg_name=$(basename $pkg)
-    ar p $pkg control.tar.gz | tar -xzOf- './control' | sed -e "s/^Description:/Filename: $pkg_name\\
-Size: $file_size\\
-MD5Sum: $md5sum\\
-Description:/" >> Packages
+    description=$(echo "$pkg_name" | sed 's/^enigma2-plugin-picons-//' | sed 's/_.*$//')
+    newversion=$(echo "$pkg" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}')
+    ar p "$pkg" control.tar.gz | tar -xzOf- './control' |
+    awk -v newver="$newversion" -v pkgname="$pkg_name" -v filesize="$file_size" -v md5="$md5sum" -v description="$description" '{
+    if ($0 ~ "^Version:") {
+      printf "Version: %s\n", newver
+    } else if ($0 ~ "^Description:") {
+      printf "Filename: %s\nSize: %s\nMD5Sum: %s\nDescription:%s\n", pkgname, filesize, md5, description
+    } else {
+      print
+    }
+  }' >> Packages
     echo "" >> Packages
     echo "" >> Packages
 done
